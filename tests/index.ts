a071e256e3f522d50a6ebf364164945007eefd76
Lars Schou <lars.sc@gmail.com>
2024-03-05 23:50:34 +0100

    Remove unnecessary test suite for openai import

--- tests/index.ts
+++ tests/index.ts
@@ -2,7 +2,6 @@ import { describe } from 'manten'
 
 describe('aicommit', ({ runTestSuite }) => {
   runTestSuite(import('./specs/cli/index.js'))
-  runTestSuite(import('./specs/openai/index.js'))
   runTestSuite(import('./specs/config.js'))
   runTestSuite(import('./specs/git-hook.js'))
 })
